d6670d8246fd23e8a6521b6a8939b8e1ed1ea13e
jstop01 <jstop01@naver.com>
2022-11-29 18:00:01 +0900

    stream을 이용한 리스트 간편 꺼내기 밑 람다 활용

--- src/java8stream/AppEx.java
+++ src/java8stream/AppEx.java
@@ -3,6 +3,7 @@ package java8stream;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Queue;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -11,9 +12,9 @@ public class AppEx {
     public static void main(String[] args) {
         List<OnlineClass> springClasses = new ArrayList<>();
         springClasses.add(new OnlineClass(1, "spring boot", true));
-        springClasses.add(new OnlineClass(2,"spring data jpa",true));
-        springClasses.add(new OnlineClass(3,"spring mvc",false));
-        springClasses.add(new OnlineClass(4,"spring core",false));
+        springClasses.add(new OnlineClass(2, "spring data jpa", true));
+        springClasses.add(new OnlineClass(3, "spring mvc", false));
+        springClasses.add(new OnlineClass(4, "spring core", false));
         springClasses.add(new OnlineClass(5, "rest api development", false));
 
         System.out.println("spring으로 시작하는 수업");
@@ -22,14 +23,14 @@ public class AppEx {
         System.out.println("close 되지 않은 수업");
         //TODO
 
+
         System.out.println("수업 이름만 모아서 스트림 만들기");
         //TODO
 
-
         List<OnlineClass> javaClasses = new ArrayList<>();
-        javaClasses.add(new OnlineClass(6,"The Java, Test", true));
-        javaClasses.add(new OnlineClass(7,"The Java, Code manipulation", true));
-        javaClasses.add(new OnlineClass(8,"The Java, 8 to 11", false));
+        javaClasses.add(new OnlineClass(6, "The Java, Test", true));
+        javaClasses.add(new OnlineClass(7, "The Java, Code manipulation", true));
+        javaClasses.add(new OnlineClass(8, "The Java, 8 to 11", false));
 
         List<List<OnlineClass>> keesunEvents = new ArrayList<>();
 
@@ -48,5 +49,7 @@ public class AppEx {
         System.out.println("스프링 수업 중에 제목에 spring이 들어간 제목만 모아서 List로 만들기");
         //TODO
 
+
     }
+
 }
