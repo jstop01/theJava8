7150bcc51c75a3cdd82463a18cc14f84c26c33bd
jstop01 <jstop01@naver.com>
2022-12-05 08:43:07 +0900

    자바8 문법 공부

--- src/java8stream/AppEx.java
+++ src/java8stream/AppEx.java
@@ -1,5 +1,6 @@
 package java8stream;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -17,39 +18,11 @@ public class AppEx {
         springClasses.add(new OnlineClass(4, "spring core", false));
         springClasses.add(new OnlineClass(5, "rest api development", false));
 
-        System.out.println("spring으로 시작하는 수업");
-        //TODO
-
-        System.out.println("close 되지 않은 수업");
-        //TODO
-
-
-        System.out.println("수업 이름만 모아서 스트림 만들기");
-        //TODO
-
-        List<OnlineClass> javaClasses = new ArrayList<>();
-        javaClasses.add(new OnlineClass(6, "The Java, Test", true));
-        javaClasses.add(new OnlineClass(7, "The Java, Code manipulation", true));
-        javaClasses.add(new OnlineClass(8, "The Java, 8 to 11", false));
-
-        List<List<OnlineClass>> keesunEvents = new ArrayList<>();
-
-        keesunEvents.add(springClasses);
-        keesunEvents.add(javaClasses);
-
-        System.out.println("두 수업 목록에 들어있는 모든 수업 아이디 출력");
-        //TODO
-
-        System.out.println("10부터 1씩 증가하는 무제한 스트림 중에서 앞에 10개 빼고 최대 10개 까지만");
-        //TODO
-
-        System.out.println("자바 수업 중에 Test가 들어있는 수업이 있는지 확인");
-        //TODO
-
-        System.out.println("스프링 수업 중에 제목에 spring이 들어간 제목만 모아서 List로 만들기");
-        //TODO
-
-
+        OnlineClass spring_boot = new OnlineClass(1,"spring boot", true);
+//        Progress progress = spring_boot.getProgress();
+//        if (progress != null) {
+//            System.out.println(progress.getStudyDuration());
+//        }
     }
 
 }
---
+++ src/java8stream/Progress.java
@@ -0,0 +1,18 @@
+package java8stream;
+
+import java.time.Duration;
+
+public class Progress {
+
+    private Duration studyDuration;
+
+    private boolean finished;
+
+    public Duration getStudyDuration() {
+        return studyDuration;
+    }
+
+    public void setStudyDuration(Duration studyDuration) {
+        this.studyDuration = studyDuration;
+    }
+}
